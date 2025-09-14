d63979f594cbebf86676b3ef81b16f61372d729d
Braxton Herold <123220726+Tchurkin@users.noreply.github.com>
2025-09-13 18:39:00 -0700

    Update Ascent_Test.cpp

--- Ascent_Test.cpp
+++ Ascent_Test.cpp
@@ -282,7 +282,7 @@ bool buttonCount() {
 }
 
 void countdown() {
-  const unsigned long countdownDuration = 30;
+  const unsigned long countdownDuration = 10;
   servoX.write(90 + Xtune);
   servoY.write(90 + Ytune);
 
@@ -323,8 +323,8 @@ void TVC() {
   sensors();
   tiltX = constrain(P * gyro_x + D * ang_vel_x, -5, 5) * ServoXMult;
   tiltY = constrain(P * gyro_y + D * ang_vel_y, -5, 5) * ServoYMult;
-  servoX.write(-tiltX + 90);
-  servoY.write(-tiltY + 90);
+  servoX.write(tiltX + 90);
+  servoY.write(tiltY + 90);
   delay(5);
 }
 
